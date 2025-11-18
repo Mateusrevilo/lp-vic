f4f2a1f95285d7373a9a4a5bf9f2e7d40149f027
jonatasmirand <jonatasmiranda97.jm@gmail.com>
2025-11-17 22:41:44 -0300

    Estilização da section

---
+++ OneDrive/Documentos/LP-vitoria/landing/app/components/Cards/DescricaoPrata/index.tsx
@@ -0,0 +1,28 @@
+import { Descricao } from "@/app/types/descricao/Descricao"
+
+type Props = {
+    descricao: Descricao
+}
+
+const DescricaoProduto = ({ descricao }: Props) => {
+
+    return (
+       <div>
+            <div>
+                <h3>{descricao.titulo}</h3>
+                <div>
+                    <ul>
+                        <li>{descricao.texto}</li>
+                        <li>{descricao.texto2}</li>
+                        <li>{descricao.texto3}</li>
+                        <li>{descricao.texto4}</li>
+                        <li>{descricao.texto5}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default DescricaoProduto
---
+++ OneDrive/Documentos/LP-vitoria/landing/app/page.tsx
@@ -0,0 +1,58 @@
+import HeroSection from "./components/HeroSection";
+import DescricaoPrata from "./components/Cards/DescricaoPrata";
+import DescricaoSemiJoia from "./components/Cards/DescricaoSemiJoia";
+import DescricaoBijuteria from "./components/Cards/DescricaoBijuteria";
+
+export default function Home() {
+
+  //Aqui estão os dados da HeroSection
+  const contentHero = {
+    title: "Conto de Pratas",
+    subtitle: "Pratas e Acessórios",
+    btnText: "Compre Agora"
+  }
+
+  //Aqui estão os cards de diferenca entre pratas e semijoias
+
+  //card de Pratas
+  const cardPrata = {
+    titulo: "Pratas",
+    texto: "Durabilidade e Investimento de Longo Prazo",
+    texto2: "Brilho e Elegância Atemporal",
+    texto3: "É Hipoalergênica",
+    texto4: "Extrema Versatilidade",
+    texto5: "Acessibilidade como Joia"
+  }
+
+  //card de semiJoia
+
+  const cardSemiJoia = {
+    titulo: "Semijoias",
+    texto: "Versatilidade para Combinar",
+    texto2: "Hipoalergenicidade",
+    texto3: "Potencial de Revitalização (Limpeza)",
+    texto4: "Acessibilidade com Status de Joia",
+    texto5: "Durabilidade Estrutural"
+  }
+
+  // Card de BiJuteria
+
+  const cardBijuteria = {
+    titulo: "Semijoias",
+    texto: "Durabilidade Extremamente Baixa",
+    texto2: "Risco Alto de Alergias e Irritações na Pele",
+    texto3: "Escurecimento Rápido e Irreversível",
+    texto4: "Falta de Sustentabilidade",
+    texto5: "Aparência e Acabamento Inferiores"
+  }
+
+
+  return (
+    <div>
+      <HeroSection hero={contentHero} />
+      <DescricaoPrata descricao={cardPrata} />
+      <DescricaoSemiJoia descricao={cardSemiJoia} />
+      <DescricaoBijuteria descricao={cardBijuteria} />
+    </div>
+  );
+}
