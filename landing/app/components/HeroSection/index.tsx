ee21f65c9a9c53b7e5ebdb975f0af37ef5434d85
jonatasmirand <jonatasmiranda97.jm@gmail.com>
2025-11-18 23:06:12 -0300

    Estilização da section, aplicando animações e responsividade

--- landing/app/components/HeroSection/index.tsx
+++ landing/app/components/HeroSection/index.tsx
@@ -1,38 +1,36 @@
 import styles from "./HeroSection.module.css";
 import Image from "next/image";
 import Vitoria from "./icon/Vitoria.png";
-import Logo from "./icon/logo-Photoroom.png"
+import Logo from "./icon/logo-Photoroom.png";
 import type { Hero } from "@/types/heros/Hero";
 
-type Props = {
-    hero: Hero;
-};
-
+type Props = { hero: Hero };
 
 const HeroSection = ({ hero }: Props) => {
     return (
         <section className={styles.hero}>
-            <Image src={Logo}
-                alt="Conto das Pratas"
-                className={styles.logo} />
-
             <div className={styles.content}>
-                <h1 className={styles.text}>Há Uma Nova História. Conheça o Conto de Pratas!</h1>
+                <Image
+                    src={Logo}
+                    alt="Conto das Pratas"
+                    className={styles.logo}
+                    priority
+                />
+
+                <h1 className={styles.text}>Há Uma Nova História</h1>
+                <h1 className={styles.text}>Conheça o Conto de Pratas!</h1>
+
                 <button className={styles.btnText}>
                     {hero.btnText}
                 </button>
             </div>
 
-            <div className={styles.person}>
-                <Image
-                    src={Vitoria}
-                    alt="Modelo"
-                    fill
-                    style={{ objectFit: "contain" }}
-                    priority
-                />
-            </div>
-
+            <Image
+                src={Vitoria}
+                alt="Modelo"
+                className={styles.person}
+                priority
+            />
         </section>
     );
 };
